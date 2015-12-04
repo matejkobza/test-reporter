2a9c264e83c5ffda5d1da54e2eeac9bb282cdd7e
Matej Kobza <mates@ravers.sk>
2015-12-04 13:35:22 +0100

    application configuration

---
+++ src/main/java/sk/trilobit/eskn/reporter/web/SampleController.java
@@ -0,0 +1,21 @@
+package sk.trilobit.eskn.reporter.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import sk.trilobit.eskn.reporter.service.HelloWorldService;
+
+@Controller
+public class SampleController {
+
+	@Autowired
+	private HelloWorldService helloWorldService;
+
+	@RequestMapping("/")
+	@ResponseBody
+	public String helloWorld() {
+		return this.helloWorldService.getHelloMessage();
+	}
+
+}
