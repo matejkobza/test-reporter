d6acaf87958621e749e69bab5673ffeb40dc4196
doloi72 <doloi72@szm.sk>
2015-12-11 14:07:21 +0100

    pom

--- src/main/webapp/scripts/app/app.controller.js
+++ src/main/webapp/scripts/app/app.controller.js
@@ -1,6 +1,18 @@
 'use strict';
 
 angular.module('app')
-    .controller('AppController', function () {
+    .controller('AppController', function ($scope) {
+
+        $scope.tab = 1;
+
+
+        $scope.testy = function() {
+            $scope.tab = 1;
+        };
+
+        $scope.logs = function() {
+            $scope.tab = 2;
+        };
+
 
     });
