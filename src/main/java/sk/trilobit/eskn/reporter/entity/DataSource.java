99b93bc851abf7ce51d1ad18fe9261810d9f8d2e
Matej Kobza <matej.kobza@embedit.cz>
2016-01-13 13:28:56 +0100

    removed JPA dependencies as they dont work right now

--- src/main/java/sk/trilobit/eskn/reporter/entity/DataSource.java
+++ src/main/java/sk/trilobit/eskn/reporter/entity/DataSource.java
@@ -1,14 +1,12 @@
 package sk.trilobit.eskn.reporter.entity;
 
-import javax.persistence.Entity;
-
 /**
  * Created with IntelliJ IDEA.
  * User: matejkobza
  * Date: 02.01.16
  * Time: 12:47
  */
-@Entity
+//@Entity
 public class DataSource {
 
 
