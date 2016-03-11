7bf71225ec228be504b86d73c5cb700ffa813687
Matej Kobza <matej.kobza@embedit.cz>
2016-03-11 09:50:02 +0100

    getter and setter anotations

--- src/main/java/sk/trilobit/eskn/reporter/entity/DataSource.java
+++ src/main/java/sk/trilobit/eskn/reporter/entity/DataSource.java
@@ -1,19 +1,12 @@
 package sk.trilobit.eskn.reporter.entity;
-import lombok.Data;
 
-import javax.persistence.*;
-/**
- * Created with IntelliJ IDEA.
- * User: matejkobza
- * Date: 02.01.16
- * Time: 12:47
- */
+import lombok.Getter;
+import lombok.Setter;
 
 //@Entity
-public @Data
-class DataSource {
-    
-
+@Getter
+@Setter
+public class DataSource {
 
     private String driverClassName;
     private String jdbcUrl;
@@ -22,9 +15,5 @@ class DataSource {
     private String databaseName;
     private String user;
     private String password;
-
-
-
-
 
 }
