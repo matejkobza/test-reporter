f2a4d9414b92173200c3565ed94b406579f06cb6
doloi72 <doloi72@szm.sk>
2016-03-11 09:23:23 +0100

    DataSource

--- src/main/java/sk/trilobit/eskn/reporter/entity/DataSource.java
+++ src/main/java/sk/trilobit/eskn/reporter/entity/DataSource.java
@@ -1,5 +1,5 @@
 package sk.trilobit.eskn.reporter.entity;
-
+import javax.persistence.*;
 /**
  * Created with IntelliJ IDEA.
  * User: matejkobza
@@ -8,6 +8,72 @@ package sk.trilobit.eskn.reporter.entity;
  */
 //@Entity
 public class DataSource {
+
+
+    private String driverClassName;
+    private String jdbcUrl;
+    private String serverName;
+    private int portNumber;
+    private String databaseName;
+    private String user;
+    private String password;
+
+    public String getDriverClassName() {
+        return driverClassName;
+    }
+
+    public void setDriverClassName(String driverClassName) {
+        this.driverClassName = driverClassName;
+    }
+
+    public String getJdbcUrl() {
+        return jdbcUrl;
+    }
+
+    public void setJdbcUrl(String jdbcUrl) {
+        this.jdbcUrl = jdbcUrl;
+    }
+
+    public String getServerName() {
+        return serverName;
+    }
+
+    public void setServerName(String serverName) {
+        this.serverName = serverName;
+    }
+
+    public int getPortNumber() {
+        return portNumber;
+    }
+
+    public void setPortNumber(int portNumber) {
+        this.portNumber = portNumber;
+    }
+
+    public String getDatabaseName() {
+        return databaseName;
+    }
+
+    public void setDatabaseName(String databaseName) {
+        this.databaseName = databaseName;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 
 
 
