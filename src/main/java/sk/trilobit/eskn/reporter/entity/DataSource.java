f0d5ca19e0da08fc872edb01f19e0f5a5b9a8544
doloi72 <doloi72@szm.sk>
2016-03-11 10:05:49 +0100

    DataSource

--- src/main/java/sk/trilobit/eskn/reporter/entity/DataSource.java
+++ src/main/java/sk/trilobit/eskn/reporter/entity/DataSource.java
@@ -3,17 +3,33 @@ package sk.trilobit.eskn.reporter.entity;
 import lombok.Getter;
 import lombok.Setter;
 
-//@Entity
+import javax.persistence.*;
+
+@Entity
 @Getter
 @Setter
+@Table(name = "DataSource_DEF")
 public class DataSource {
 
-    private String driverClassName;
+    @Column(name = "type")
     private String jdbcUrl;
+    
+    @Column(name = "driverType")
+    private String driverClassName;
+
+    @Column(name = "serverName")
     private String serverName;
+
+    @Column(name = "portNumber")
     private String portNumber;
+
+    @Column(name = "databaseName")
     private String databaseName;
+
+    @Column(name = "user")
     private String user;
+
+    @Column(name = "password")
     private String password;
 
 }
