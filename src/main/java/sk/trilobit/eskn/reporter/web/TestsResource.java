4ab30b6574232ca477284b9a697aab56cb54a73f
Matej Kobza <matej.kobza@embedit.cz>
2016-04-08 13:00:39 +0200

    orika mapper

--- src/main/java/sk/trilobit/eskn/reporter/web/TestsResource.java
+++ src/main/java/sk/trilobit/eskn/reporter/web/TestsResource.java
@@ -1,5 +1,6 @@
 package sk.trilobit.eskn.reporter.web;
 
+import ma.glasnost.orika.MapperFacade;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 import sk.trilobit.eskn.reporter.Application;
 import sk.trilobit.eskn.reporter.entity.Test;
 import sk.trilobit.eskn.reporter.repository.TestRepository;
-import sk.trilobit.eskn.reporter.service.ITestService;
 import sk.trilobit.eskn.reporter.web.dto.TestDTO;
 
 import javax.inject.Inject;
@@ -22,21 +22,25 @@ public class TestsResource {
 	@Inject
 	private TestRepository testRepository;
 
-	@Inject
-	private ITestService testService;
+    @Inject
+    private MapperFacade mapperFacade;
 
 	@RequestMapping(value = "/load", method = RequestMethod.GET)
-	public @ResponseBody List<Test> findTest() {
-		return testRepository.findAll();
-	}
+    public
+    @ResponseBody
+    List<TestDTO> findTest() {
+        List<TestDTO> testDTOs = mapperFacade.mapAsList(testRepository.findAll(), TestDTO.class);
+        return testDTOs;
+    }
 
 	@Transactional
 	@RequestMapping(value = "/save", method = RequestMethod.POST)
 	public
 	@ResponseBody
-	List<Test> saveTest(@RequestBody TestDTO test) {
-		this.testService.save(test);
-		return testRepository.findAll();
-	}
+    List<TestDTO> saveTest(@RequestBody TestDTO testDTO) {
+        Test t = mapperFacade.map(testDTO, Test.class);
+        this.testRepository.save(t);
+        return findTest();
+    }
 
 }
